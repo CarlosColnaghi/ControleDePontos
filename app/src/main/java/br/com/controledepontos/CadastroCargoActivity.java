3590ff8e69782c34b910d3d55c3caac375136332
Carlos Colnaghi <carlos.colnaghi@hotmail.com>
2020-10-09 22:04:35 -0300

    Finalizando as telas de cadastro e cadastro de cargo

---
+++ app/src/main/java/br/com/controledepontos/CadastroCargoActivity.java
@@ -0,0 +1,14 @@
+package br.com.controledepontos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroCargoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_cargo);
+    }
+}
--- app/src/main/java/br/com/controledepontos/InicioActivity.java
+++ app/src/main/java/br/com/controledepontos/InicioActivity.java
@@ -2,7 +2,10 @@ package br.com.controledepontos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InicioActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class InicioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        Button btnCadastroCargo = findViewById(R.id.btnCadastroCargo);
+        btnCadastroCargo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent abrirCadastroCargoActivity = new Intent(InicioActivity.this, CadastroCargoActivity.class);
+                startActivity(abrirCadastroCargoActivity);
+            }
+        });
+
     }
 }
