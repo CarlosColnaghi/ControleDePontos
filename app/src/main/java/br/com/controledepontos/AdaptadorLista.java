e404adda725b923b59105b91c0b49a9095af113a
Carlos Colnaghi <carlos.colnaghi@hotmail.com>
2020-10-28 21:46:49 -0300

    Desenvolvimento do adaptador da lista

---
+++ app/src/main/java/br/com/controledepontos/AdaptadorLista.java
@@ -0,0 +1,82 @@
+package br.com.controledepontos;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AdaptadorLista extends BaseAdapter{
+
+    public ArrayList<HashMap<String, String>> list;
+    Activity activity;
+    public static final String FIRST_COLUMN="First";
+    public static final String SECOND_COLUMN="Second";
+    public static final String THIRD_COLUMN="Third";
+    public static final String FOURTH_COLUMN="Fourth";
+
+    public AdaptadorLista(Activity activity,ArrayList<HashMap<String, String>> list){
+        super();
+        this.activity=activity;
+        this.list=list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    private class ViewHolder{
+        TextView txtFirst;
+        TextView txtSecond;
+        TextView txtThird;
+        TextView txtFourth;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder;
+
+        LayoutInflater inflater=activity.getLayoutInflater();
+
+        if(convertView == null){
+
+            convertView=inflater.inflate(R.layout.lista_consulta_ponto, null);
+            holder=new ViewHolder();
+
+            holder.txtFirst=(TextView) convertView.findViewById(R.id.primeiraColuna);
+            holder.txtSecond=(TextView) convertView.findViewById(R.id.segundaColuna);
+            holder.txtThird=(TextView) convertView.findViewById(R.id.terceiraColuna);
+            holder.txtFourth=(TextView) convertView.findViewById(R.id.quartaColuna);
+
+            convertView.setTag(holder);
+        }else{
+
+            holder=(ViewHolder) convertView.getTag();
+        }
+
+        HashMap<String, String> map=list.get(position);
+        holder.txtFirst.setText(map.get(FIRST_COLUMN));
+        holder.txtSecond.setText(map.get(SECOND_COLUMN));
+        holder.txtThird.setText(map.get(THIRD_COLUMN));
+        holder.txtFourth.setText(map.get(FOURTH_COLUMN));
+
+        return convertView;
+    }
+
+}
--- app/src/main/java/br/com/controledepontos/ConsultaPontoActivity.java
+++ app/src/main/java/br/com/controledepontos/ConsultaPontoActivity.java
@@ -1,21 +1,35 @@
 package br.com.controledepontos;
 
+import android.os.Bundle;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ConsultaPontoActivity extends AppCompatActivity {
+    public static final String FIRST_COLUMN="First";
+    public static final String SECOND_COLUMN="Second";
+    public static final String THIRD_COLUMN="Third";
+    public static final String FOURTH_COLUMN="Fourth";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lista_consulta_ponto);
+        setContentView(R.layout.activity_consulta_ponto);
 
+        ListView lista = findViewById(R.id.ltvLista);
+        ArrayList<HashMap<String, String>> dados = new ArrayList<HashMap<String,String>>();
+        HashMap<String,String> hashmap = new HashMap<String, String>();
+        hashmap.put(FIRST_COLUMN, "Allo messaging");
+        hashmap.put(SECOND_COLUMN, "google");
+        hashmap.put(THIRD_COLUMN, "Free");
+        hashmap.put(FOURTH_COLUMN, "App");
+        dados.add(hashmap);
+        AdaptadorLista adaptador = new AdaptadorLista(this, dados);
+
+        lista.setAdapter(adaptador);
 
     }
 }
