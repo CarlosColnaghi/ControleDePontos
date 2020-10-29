09292055799c974a864636c26ca24e10ed2f24a0
Carlos Colnaghi <carlos.colnaghi@hotmail.com>
2020-10-29 15:23:44 -0300

    Finalizando o layout de Consulta de Ponto

--- app/src/main/java/br/com/controledepontos/ConsultaPontoActivity.java
+++ app/src/main/java/br/com/controledepontos/ConsultaPontoActivity.java
@@ -1,6 +1,8 @@
 package br.com.controledepontos;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,7 +20,13 @@ public class ConsultaPontoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consulta_ponto);
-
+        ImageView imgVoltar = findViewById(R.id.imgVoltar);
+        imgVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
         ListView lista = findViewById(R.id.ltvLista);
         ArrayList<HashMap<String, String>> dados = new ArrayList<HashMap<String,String>>();
         HashMap<String,String> hashmap = new HashMap<String, String>();
@@ -26,7 +34,9 @@ public class ConsultaPontoActivity extends AppCompatActivity {
         hashmap.put(SECOND_COLUMN, "09:00");
         hashmap.put(THIRD_COLUMN, "19:00");
         hashmap.put(FOURTH_COLUMN, "01:00");
-        dados.add(hashmap);
+        for(int i = 0; i < 100; i++){
+            dados.add(hashmap);
+        }
         AdaptadorLista adaptador = new AdaptadorLista(this, dados);
 
         lista.setAdapter(adaptador);
