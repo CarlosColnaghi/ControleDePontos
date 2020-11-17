bd9931beedd2c95bcfefc98e1e1be0f35a4500ad
Carlos Colnaghi <carlos.colnaghi@hotmail.com>
2020-11-17 16:57:07 -0300

    Finalizando sistema de login e validação de campos

--- app/src/main/java/br/com/controledepontos/CadastroActivity.java
+++ app/src/main/java/br/com/controledepontos/CadastroActivity.java
@@ -3,11 +3,13 @@ package br.com.controledepontos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import br.com.controledepontos.dao.CargoDAO;
 import br.com.controledepontos.dao.FuncionarioDAO;
@@ -16,7 +18,11 @@ import br.com.controledepontos.model.Funcionario;
 import br.com.controledepontos.model.Turno;
 
 public class CadastroActivity extends AppCompatActivity {
-
+    private EditText edtNomeCompleto;
+    private EditText edtUsuario;
+    private EditText edtEmail;
+    private EditText edtSenha;
+    private EditText edtConfimacaoSenha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,19 +34,48 @@ public class CadastroActivity extends AppCompatActivity {
                 finish();
             }
         });
-        final EditText edtNomeCompleto = findViewById(R.id.edtNomeCompleto);
-        final EditText edtUsuario = findViewById(R.id.edtUsuario);
-        final EditText edtEmail = findViewById(R.id.edtEmail);
-        final EditText edtSenha = findViewById(R.id.edtSenha);
-        EditText edtConfirmacaoSenha = findViewById(R.id.edtConfimacaoSenha);
+
+        edtNomeCompleto = findViewById(R.id.edtNomeCompleto);
+        edtUsuario = findViewById(R.id.edtUsuario);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtSenha = findViewById(R.id.edtSenha);
+        edtConfimacaoSenha = findViewById(R.id.edtConfimacaoSenha);
+
         Button btnCadastarUsuario = findViewById(R.id.btnCadastrarUsuario);
         btnCadastarUsuario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Cargo cargo = new Cargo(1, "Cargo", Turno.MANHA);
-                Funcionario funcionario = new Funcionario(null, edtUsuario.getText().toString(), edtSenha.getText().toString(), edtEmail.getText().toString(), edtNomeCompleto.getText().toString(), cargo);
-                new FuncionarioDAO(CadastroActivity.this).inserir(funcionario);
+                if(validar()){
+                    Cargo cargo = new Cargo(1, "Cargo", Turno.MANHA);
+                    Funcionario funcionario = new Funcionario(null, edtUsuario.getText().toString(), edtSenha.getText().toString(), edtEmail.getText().toString(), edtNomeCompleto.getText().toString(), cargo);
+                    new FuncionarioDAO(CadastroActivity.this).inserir(funcionario);
+                    Toast.makeText(CadastroActivity.this, "Usuário cadastrado com sucesso", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+    boolean validar(){
+        if(TextUtils.isEmpty(edtNomeCompleto.getText().toString())){
+            edtNomeCompleto.setError("O campo " + edtNomeCompleto.getHint().toString() + " precisa ser preenchido");
+            edtNomeCompleto.requestFocus();
+        }else if(TextUtils.isEmpty(edtUsuario.getText().toString())){
+            edtUsuario.setError("O campo " + edtUsuario.getHint().toString() + " precisa ser preenchido");
+            edtUsuario.requestFocus();
+        }else if(TextUtils.isEmpty(edtEmail.getText().toString())) {
+            edtEmail.setError("O campo " + edtEmail.getHint().toString() + " precisa ser preenchido");
+            edtEmail.requestFocus();
+        }else if(TextUtils.isEmpty(edtSenha.getText().toString())){
+            edtSenha.setError("O campo " + edtSenha.getHint().toString() + " precisa ser preenchido");
+            edtSenha.requestFocus();
+        }else if(TextUtils.isEmpty(edtConfimacaoSenha.getText().toString())){
+            edtConfimacaoSenha.setError("Campo " + edtConfimacaoSenha.getHint().toString() + " precisa ser preenchido");
+            edtConfimacaoSenha.requestFocus();
+        }else if(!edtSenha.getText().toString().equals(edtConfimacaoSenha.getText().toString())){
+            edtConfimacaoSenha.setError("Os campos "+ edtSenha.getHint().toString() + " e " + edtConfimacaoSenha.getHint().toString() + " precisam ser iguais");
+            edtConfimacaoSenha.requestFocus();
+        }else{
+            return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/br/com/controledepontos/MainActivity.java
+++ app/src/main/java/br/com/controledepontos/MainActivity.java
@@ -4,22 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import br.com.controledepontos.dao.FuncionarioDAO;
+import br.com.controledepontos.model.Funcionario;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtUsuarioLogin;
+    private EditText edtSenhaLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        edtUsuarioLogin = findViewById(R.id.edtUsuarioLogin);
+        edtSenhaLogin = findViewById(R.id.edtSenhaLogin);
+
         Button btnEntrar = findViewById(R.id.btnEntrar);
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent abrirInicioActivity = new Intent(MainActivity.this, InicioActivity.class);
-                startActivity(abrirInicioActivity);
+                if (validar()){
+                    Funcionario funcionario = new FuncionarioDAO(MainActivity.this).login(edtUsuarioLogin.getText().toString(), edtSenhaLogin.getText().toString());
+                    if(funcionario != null){
+                        Intent abrirInicioActivity = new Intent(MainActivity.this, InicioActivity.class);
+                        startActivity(abrirInicioActivity);
+                        edtUsuarioLogin.setText(null);
+                        edtSenhaLogin.setText(null);
+                    }else{
+                        Toast.makeText(MainActivity.this, edtUsuarioLogin.getHint().toString() +" e/ou " + edtSenhaLogin.getHint().toString() + " estão incorretas", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
 
@@ -31,6 +51,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(abrirCadastroActivity);
             }
         });
-
+    }
+    boolean validar(){
+        if(TextUtils.isEmpty(edtUsuarioLogin.getText().toString())){
+            edtUsuarioLogin.setError("O campo " + edtUsuarioLogin.getHint().toString() + " precisa ser preenchido");
+            edtUsuarioLogin.requestFocus();
+        }else if(TextUtils.isEmpty(edtSenhaLogin.getText().toString())){
+            edtSenhaLogin.setError("O campo " + edtSenhaLogin.getHint().toString() + " precisa ser preenchido");
+            edtSenhaLogin.requestFocus();
+        }else{
+            return true;
+        }
+        return false;
     }
 }
